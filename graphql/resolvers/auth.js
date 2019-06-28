a9ade03374a7498093e3b9f9e2d8d7c856c2028a
Suchith Hegde <suchith.hegde@perficient.com>
2019-06-28 16:32:37 -0400

    chapter10 generate jwt token

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   // resolver
@@ -29,5 +30,20 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+    if(!user) {
+      throw new Error('User email doesnot exist!');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error('Password is incorrect!!');
+    }
+    const token = jwt.sign({ userId: user.id, email: user.email}, 'somesupersecretkey', {
+      expiresIn: '1h'
+    });
+    return { userId: user.id, token: token, tokenExpiration: 1 };
+
   }
 };
