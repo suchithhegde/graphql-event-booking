0c86e633adba23911a4b73b4fbf299d677f20e63
Suchith Hegde <suchith.hegde@perficient.com>
2019-07-07 15:27:48 -0400

    chapter13

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
 const isAuth = require('./middleware/is-auth');
-
 const graphQLSchema = require('./graphql/schema/index');
 const graphQLResolvers = require('./graphql/resolvers/index');
+
 const app = express();
 // not needed since storing into mongoose
 //const events = [];
-app.get('/', (req, res, next) => {
-  res.send('Hello World!!');
-})
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(isAuth);
 
@@ -30,7 +37,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
 }@prft-cluster-a4xjn.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, {
     useNewUrlParser: true
   }).then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000.....'));
+    app.listen(8000, () => console.log('Listening backend on port 8000.....'));
   })
   .catch(err => {
     console.log(err);
